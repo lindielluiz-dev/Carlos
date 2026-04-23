caea4502423a2ee259e13d3f838c6d4d5935a290
lindielluiz-dev <lindielluiz@gmail.com>
2026-04-23 08:36:16 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function mudar_conteudo_elemento() {
-
-    document.getElementById("h1").innerHTML = "Hello JavaScript!";
-}
-function mudar_estilo(){
-
-document.getElementById("paragrafo").style.textAlign = "right";
-}
