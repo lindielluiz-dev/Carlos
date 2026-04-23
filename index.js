9307eeac9695f4b8cc7df308edc549c9386f4e5c
lindielluiz-dev <lindielluiz@gmail.com>
2026-04-23 08:53:43 -0300

    confirmando configuração do github

---
+++ index.js
@@ -0,0 +1,8 @@
+function mudar_conteudo_elemento() {
+
+    document.getElementById("h1").innerHTML = "Hello JavaScript!";
+}
+function mudar_estilo(){
+
+document.getElementById("paragrafo").style.textAlign = "right";
+}
